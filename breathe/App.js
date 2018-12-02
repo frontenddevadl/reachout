6d3f3b5d38226d0771855bc7576489d8a90ccf65
Pigram <mathew.pigram@capgemini.com>
2018-12-02 11:10:38 +1030

    implemented the settings store using mobx

--- breathe/App.js
+++ breathe/App.js
@@ -2,11 +2,16 @@ import React from 'react';
 import { Platform, StatusBar, StyleSheet, View } from 'react-native';
 import { AppLoading, Asset, Font, Icon } from 'expo';
 import AppNavigator from './navigation/AppNavigator';
+import settingsStore from './mobx/settings'
 
 export default class App extends React.Component {
   state = {
     isLoadingComplete: false,
   };
+
+  componentDidMount() {
+    settingsStore.loadSettings();
+  }
 
   render() {
     if (!this.state.isLoadingComplete && !this.props.skipLoadingScreen) {
@@ -21,7 +26,9 @@ export default class App extends React.Component {
       return (
         <View style={styles.container}>
           {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
-          <AppNavigator />
+          <AppNavigator screenProps={{
+            settingsStore: settingsStore
+          }}/>
         </View>
       );
     }
